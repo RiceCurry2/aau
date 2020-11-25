1d3c7d01380ea4dcc30c246cf751ba591373ca50
Kjartan (Charlie) Nolsøe Jespersen <72762449+knje20@users.noreply.github.com>
2020-11-25 19:09:29 +0100

    Adding to the scan function
    Added information about the 90 degree scan area:
       at angle -45 = furthest left array place [639]
       at angle 45 = furthest right array place [0]

--- rpws/src/project_ros/include/pr_scan/PRScan.hpp
+++ rpws/src/project_ros/include/pr_scan/PRScan.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <std_msgs/String.h>
+
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 
@@ -17,6 +19,10 @@ class PRScan
     private:
 
     ros::NodeHandle& nodeHandle_;
+    ros::Subscriber subscriber_;
+    std::string subscriberTopic_ = "/scan";
+
+    void topicCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
 };
 
 } /*namespace*/
--- rpws/src/project_ros/src/PRScan.cpp
+++ rpws/src/project_ros/src/PRScan.cpp
@@ -5,7 +5,28 @@ namespace pr_scan{
     PRScan::PRScan(ros::NodeHandle& nodeHandle)
         :nodeHandle_(nodeHandle)
     {
+        subscriber_ = nodeHandle_.subscribe<sensor_msgs::LaserScan>(subscriberTopic_, 10,
+                                    &PRScan::topicCallback, this);
+        ROS_INFO("Sucessfully launched node.");
 
+        ros::MultiThreadedSpinner spinner(1);
+
+        spinner.spin();
+        
+    }
+
+    void PRScan::topicCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+    {
+        float scanImage[640];
+
+        float xRight = msg->ranges.at(0);       // Furthes right scanner is capturing
+        float xCenter = msg->ranges.at(319);    // Center
+        float xLeft = msg->ranges.at(639);      // Furthest left scanner is capturing
+
+        //float x = msgsensor_msgs::LaserScan range_data;
+        //scanImage[0] = range_data.ranges.at(1);
+
+        ROS_INFO("Scan info: Left: %f, Center: %f Right: %f", xLeft, xCenter, xRight);//scanImage[0]);
     }
 
 
